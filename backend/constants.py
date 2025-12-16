58c68bbc2b75f12a06dad53a7625868ab63f3980
Gustavo <gustavoconect@github.com>
2025-12-16 19:58:22 -0300

    chore: update champion lane mapping in backend

--- backend/constants.py
+++ backend/constants.py
@@ -16,36 +16,44 @@ ELO_HIERARCHY = {
 LANES = ["Top", "Jungle", "Mid", "ADC", "Support"]
 
 # Champion Lane Mapping (Season 14/15 Meta)
+# Champion Lane Mapping (Based on provided analysis)
 LANE_CHAMPIONS = {
     "Top": [
-        "Aatrox", "Akali", "Ambessa", "Aurora", "Camille", "Cho'Gath", "Darius", "Dr. Mundo", "Fiora", "Gangplank", 
-        "Garen", "Gnar", "Gragas", "Gwen", "Heimerdinger", "Illaoi", "Irelia", "Jax", "Jayce", "K'Sante", "Kayle", 
-        "Kennen", "Kled", "Malphite", "Maokai", "Mel", "Mordekaiser", "Nasus", "Olaf", "Ornn", "Pantheon", "Poppy", 
-        "Quinn", "Renekton", "Riven", "Rumble", "Sett", "Shen", "Singed", "Sion", "Tahm Kench", "Teemo", 
-        "Tryndamere", "Urgot", "Vayne", "Volibear", "Warwick", "Wukong", "Yasuo", "Yone", "Yorick", "Zaahen"
+        "Aatrox", "Akali", "Ambessa", "Camille", "Cassiopeia", "Cho'Gath", "Darius", "Dr. Mundo", 
+        "Fiora", "Gangplank", "Garen", "Gnar", "Gragas", "Gwen", "Heimerdinger", "Illaoi", 
+        "Irelia", "Jax", "Jayce", "K'Sante", "Kayle", "Kennen", "Kled", "Malphite", "Mordekaiser", 
+        "Nasus", "Olaf", "Ornn", "Pantheon", "Poppy", "Quinn", "Renekton", "Riven", "Rumble", 
+        "Ryze", "Sett", "Shen", "Singed", "Sion", "Tahm Kench", "Teemo", "Trundle", "Tryndamere", 
+        "Urgot", "Vayne", "Vladimir", "Volibear", "Warwick", "Wukong", "Yasuo", "Yone", "Yorick", 
+        "Zaahen"
     ],
     "Jungle": [
-        "Amumu", "Bel'Veth", "Briar", "Diana", "Ekko", "Elise", "Evelynn", "Fiddlesticks", "Graves", "Hecarim", 
-        "Ivern", "Jarvan IV", "Karthus", "Kayn", "Kha'Zix", "Kindred", "Lee Sin", "Lillia", "Master Yi", "Nidalee", 
-        "Nocturne", "Nunu & Willump", "Rammus", "Rek'Sai", "Rengar", "Sejuani", "Shaco", "Shyvana", "Skarner", 
-        "Taliyah", "Trundle", "Udyr", "Vi", "Viego", "Volibear", "Warwick", "Xin Zhao", "Zac", "Zed"
+        "Aatrox", "Ambessa", "Amumu", "Bel'Veth", "Briar", "Cho'Gath", "Diana", "Dr. Mundo", 
+        "Ekko", "Elise", "Evelynn", "Fiddlesticks", "Gragas", "Graves", "Gwen", "Hecarim", "Ivern", 
+        "Jarvan IV", "Jax", "Jayce", "Karthus", "Kayn", "Kha'Zix", "Kindred", "Lee Sin", "Lillia", 
+        "Malphite", "Master Yi", "Naafiri", "Nidalee", "Nocturne", "Nunu e Willump", "Pantheon", 
+        "Qiyana", "Rammus", "Rek'Sai", "Rengar", "Sejuani", "Shaco", "Shyvana", "Skarner", "Sylas", 
+        "Talon", "Teemo", "Trundle", "Udyr", "Vi", "Viego", "Volibear", "Warwick", "Wukong", 
+        "Xin Zhao", "Zaahen", "Zac", "Zed"
     ],
     "Mid": [
-        "Ahri", "Akali", "Akshan", "Anivia", "Annie", "Aurelion Sol", "Aurora", "Azir", "Cassiopeia", "Corki", 
-        "Diana", "Ekko", "Fizz", "Galio", "Hwei", "Irelia", "Jayce", "Kassadin", "Katarina", "LeBlanc", "Lissandra", 
-        "Lux", "Malzahar", "Mel", "Naafiri", "Neeko", "Orianna", "Qiyana", "Ryze", "Smolder", "Sylas", "Syndra", 
-        "Taliyah", "Talon", "Twisted Fate", "Veigar", "Vex", "Viktor", "Vladimir", "Xerath", "Yasuo", "Yone", 
-        "Zed", "Ziggs", "Zoe"
+        "Ahri", "Akali", "Akshan", "Anivia", "Annie", "Aurelion Sol", "Aurora", "Azir", "Brand", 
+        "Cassiopeia", "Diana", "Ekko", "Fizz", "Galio", "Heimerdinger", "Hwei", "Irelia", 
+        "Kassadin", "Katarina", "Kayle", "Kennen", "LeBlanc", "Lissandra", "Lux", "Malzahar", 
+        "Mel", "Morgana", "Naafiri", "Orianna", "Qiyana", "Ryze", "Smolder", "Swain", "Sylas", 
+        "Syndra", "Taliyah", "Talon", "Twisted Fate", "Veigar", "Vel'Koz", "Vex", "Viktor", 
+        "Vladimir", "Xerath", "Yasuo", "Yone", "Zed", "Ziggs", "Zoe"
     ],
     "ADC": [
-        "Aphelios", "Ashe", "Caitlyn", "Draven", "Ezreal", "Jhin", "Jinx", "Kai'Sa", "Kalista", "Kog'Maw", 
-        "Lucian", "Miss Fortune", "Nilah", "Samira", "Sivir", "Smolder", "Tristana", "Twitch", "Varus", "Vayne", 
-        "Xayah", "Yunara", "Zeri", "Ziggs", "Seraphine", "Swain", "Yasuo"
+        "Aphelios", "Ashe", "Caitlyn", "Corki", "Draven", "Ezreal", "Jhin", "Jinx", "Kai'Sa", 
+        "Kalista", "Kog'Maw", "Lucian", "Miss Fortune", "Nilah", "Samira", "Sivir", "Smolder", 
+        "Tristana", "Twitch", "Varus", "Vayne", "Xayah", "Yunara", "Zeri", "Ziggs"
     ],
     "Support": [
-        "Alistar", "Bard", "Blitzcrank", "Brand", "Braum", "Janna", "Karma", "Leona", "Lulu", "Lux", 
-        "Maokai", "Mel", "Milio", "Morgana", "Nami", "Nautilus", "Pyke", "Rakan", "Rell", "Renata Glasc", "Senna", 
-        "Seraphine", "Sona", "Soraka", "Swain", "Tahm Kench", "Taric", "Thresh", "Vel'Koz", "Xerath", "Yuumi", 
-        "Zilean", "Zyra", "Camille", "Poppy"
+        "Alistar", "Bardo", "Blitzcrank", "Brand", "Braum", "Elise", "Fiddlesticks", "Janna", 
+        "Karma", "Leona", "Lulu", "Lux", "Maokai", "Mel", "Milio", "Morgana", "Nami", "Nautilus", 
+        "Neeko", "Pantheon", "Poppy", "Pyke", "Rakan", "Rell", "Renata Glasc", "Senna", 
+        "Seraphine", "Shaco", "Shen", "Sona", "Soraka", "Swain", "Tahm Kench", "Taric", "Teemo", 
+        "Thresh", "Vel'Koz", "Xerath", "Yuumi", "Zilean", "Zoe", "Zyra"
     ]
 }
